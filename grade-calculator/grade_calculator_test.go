f4568ec01da8b852f4be53894173d6b2d1ca5877
Nattanon Nisup <sam_zheza@hotmail.com>
2025-09-28 12:07:55 -0400

    fix+test: correct essay averaging and fix TestGetGradeF inputs

--- grade-calculator/grade_calculator_test.go
+++ grade-calculator/grade_calculator_test.go
@@ -39,9 +39,9 @@ func TestGetGradeF(t *testing.T) {
 
 	gradeCalculator := NewGradeCalculator()
 
-	gradeCalculator.AddGrade("open source assignment", 100, Assignment)
-	gradeCalculator.AddGrade("exam 1", 95, Exam)
-	gradeCalculator.AddGrade("essay on ai ethics", 91, Essay)
+	gradeCalculator.AddGrade("open source assignment", 30, Assignment)
+	gradeCalculator.AddGrade("exam 1", 40, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 20, Essay)
 
 	actual_value := gradeCalculator.GetFinalGrade()
 
